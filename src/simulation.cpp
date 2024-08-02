bedd16dacc678d85dacead700d78beac95cb2301
Matheus <mhcoelhosilva@gmail.com>
2024-08-02 10:54:04 +0100

    Whitespace

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -28,16 +28,19 @@ void Simulation::RunSimulation() {
         }
         break;
     }
+
     std::random_device os_seed;
     const uint32_t seed = os_seed();
     engine generator(seed);
     std::uniform_int_distribution<uint32_t> distribute(0, 1);
     auto coinToss = std::bind(distribute, generator);
+    
     for (int t = 0; t < m_args.m_duration; ++t) {
         for (size_t i = 0; i < m_money.size() - 1; ++i) {
             for (size_t j = i + 1; j < m_money.size(); ++j) {
                 if (m_args.m_type == SimulationType::CantTradeAtZero && (m_money[j] == 0 || m_money[i] == 0))
                     continue;
+
                 auto res = coinToss();
                 if (res == 0) {
                     if (m_args.m_type == SimulationType::CanOnlyReceiveAtZero && m_money[i] == 0)
@@ -55,6 +58,7 @@ void Simulation::RunSimulation() {
             }
         }
     }
+
     int numAboveZero = 0;
     for (auto m : m_money) {
         if (m > 0) {
@@ -73,11 +77,13 @@ void Simulation::SaveOutputToJson() {
     j["m_duration"] = m_args.m_duration;
     j["m_type"] = m_args.m_type == SimulationType::CantTradeAtZero ? "cant_trade" : "can_receive";
     j["final_data"] = m_money;
+
     string filename = "../data/output_";
     filename += to_string(m_money.size()) + "_";
     filename += to_string(m_args.m_initialAmount) + "_";
     filename += to_string(m_args.m_duration) + "_";
     filename += to_string(m_args.m_type) + ".json";
+
     std::ofstream o;
     o.open(filename);
     o << j.dump(4) << std::endl;
